17c810ef00d30fe9ca1ab18727e01d89ffe8aee6
EXP <289065406@qq.com>
2023-05-09 11:01:18 +0800

    add music player

--- py/gen_music_list.py
+++ py/gen_music_list.py
@@ -1,7 +1,19 @@
-import os
-import json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# env: python3
+# --------------------------------------------
+# 生成仓库所有音乐文件的路径，供播放器读取
+#  （可人工执行、亦可通过 Github Action 在 PR 时触发）
+# --------------------------------------------
+# usage: 
+#   python ./py/gen_music_list.py
+# --------------------------------------------
 
-MUSIC_DIR = './static'
+
+import os
+
+WORK_DIR = "."
+MUSIC_DIR = f"{WORK_DIR}/static"
 MUSIC_LIST = f"{MUSIC_DIR}/music_list.dat"
 
 music_list = []
@@ -9,7 +21,7 @@ for root, _, files in os.walk(MUSIC_DIR):
     for file in files:
         if not file.lower().endswith('.mp3'):
             continue
-        rel_path = os.path.relpath(os.path.join(root, file), MUSIC_DIR)
+        rel_path = os.path.relpath(os.path.join(root, file), WORK_DIR)
         music_list.append(rel_path.replace("\\", "/"))
 
 with open(MUSIC_LIST, 'w', encoding='utf-8') as file:
