018e53a49355180f408aae9cf4c96b63b4e30ec4
EXP <289065406@qq.com>
2023-06-11 13:31:14 +0800

    脚本优化

--- py/fix_metadata.py
+++ py/fix_metadata.py
@@ -13,7 +13,6 @@ import os
 from mutagen.id3 import ID3, TPE1, TALB, TIT2
 from color_log.clog import log
 
-DEFAULT_ENCODING = "utf-8"
 WORK_DIR = "."
 MUSIC_DIR = f"{WORK_DIR}/static"
 MUSIC_SUFFIXES = [ ".mp3", ".wma" ]
@@ -37,9 +36,10 @@ def main() :
 def fix_meta_to_utf8(music_path, music_name) :
     try:
         audio = ID3(music_path)
-        fix_title(audio, music_name)
-        fix_artist(audio)
-        fix_album(audio)
+        album, artist, title = take_metadate(music_name)
+        fix_title(audio, title)
+        fix_artist(audio, artist)
+        fix_album(audio, album)
 
         audio.save()  # 保存修改后的元数据
         log.debug(f"修复歌曲元数据成功： {music_name}")
@@ -50,48 +50,70 @@ def fix_meta_to_utf8(music_path, music_name) :
 
 
 # 修正 标题 元数据
-def fix_title(audio, music_name) :
+def fix_title(audio, default_title) :
     try:
         title_frames = audio.getall('TIT2')  # 获取所有 'TIT2' 帧（标题）
-        title = ""
-        if title_frames :
-            title = decode_gibberish(title_frames)
-            title = title.strip()
-        if title == "" or title == "<未知标题>" :          # 使用文件名作为标题
-            audio.add(TIT2(encoding=3, text=music_name))  # 添加新的 'TIT2' 帧
+        title = decode_gibberish(title_frames)
+        if title == "" or title == "<未知标题>" :
+            audio.delall('TIT2')                            # 删除所有 'TPE1' 帧
+            audio.add(TIT2(encoding=3, text=default_title))    # 添加新的 'TIT2' 帧
     except:
-        # 正确的元数据因为无法解码报错，无需处理
-        pass
+        pass    # 正确的元数据因为无法解码报错，无需处理
+
 
 # 修正 艺术家 元数据
-def fix_artist(audio) :
+def fix_artist(audio, default_artist) :
     try:
         artist_frames = audio.getall('TPE1')  # 获取所有 'TPE1' 帧（艺术家）
-        if artist_frames:
-            artist = decode_gibberish(artist_frames)
-            audio.delall('TPE1')  # 删除所有 'TPE1' 帧
-            audio.add(TPE1(encoding=3, text=artist))  # 添加新的 'TPE1' 帧
+        artist = decode_gibberish(artist_frames)
+        if artist == "" or artist == "<未知艺术家>" :
+            audio.delall('TPE1')                                # 删除所有 'TPE1' 帧
+            audio.add(TPE1(encoding=3, text=default_artist))    # 添加新的 'TPE1' 帧
     except:
-        # 正确的元数据因为无法解码报错，无需处理
-        pass
+        pass    # 正确的元数据因为无法解码报错，无需处理
 
 
 # 修正 专辑 元数据
-def fix_album(audio) :
+def fix_album(audio, default_album) :
     try:
         album_frames = audio.getall('TALB')  # 获取所有 'TALB' 帧（专辑）
-        if album_frames:
-            album = decode_gibberish(album_frames)
-            audio.delall('TALB')  # 删除所有 'TALB' 帧
-            audio.add(TALB(encoding=3, text=album))  # 添加新的 'TALB' 帧
+        album = decode_gibberish(album_frames)
+        if album == "" or album == "<未知唱片集>" :
+            audio.delall('TALB')                                # 删除所有 'TALB' 帧
+            audio.add(TALB(encoding=3, text=default_album))     # 添加新的 'TALB' 帧
     except:
-        # 正确的元数据因为无法解码报错，无需处理
-        pass
+        pass    # 正确的元数据因为无法解码报错，无需处理
 
 
 # 乱码解码（以 GBK 存储的元数据会乱码）
 def decode_gibberish(frames) :
-    return frames[0].text[0].encode('iso-8859-1').decode('gbk')
+    text = ""
+    if frames :
+        text = frames[0].text[0].encode('iso-8859-1').decode('gbk')
+        text = text.strip()
+    return text
+
+
+# 从文件名提取元数据
+def take_metadate(music_name) :
+    title = ""
+    artist = ""
+    album = ""
+    metas = music_name.split('-')
+
+    if len(metas) == 3 :
+        album = metas[0].strip()
+        artist = metas[1].strip()
+        title = metas[2].strip()
+
+    elif len(metas) == 2 :
+        artist = metas[0].strip()
+        title = metas[1].strip()
+
+    else :
+        title = music_name
+
+    return (album, artist, title)
 
     
 
