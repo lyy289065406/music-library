575cefda3e8f47301df003184e9b2e9c097ecdc9
EXP <289065406@qq.com>
2023-06-11 11:16:37 +0800

    更新歌单

--- py/gen_music_list.py
+++ py/gen_music_list.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 # env: python3
 # --------------------------------------------
-# 生成仓库所有音乐文件的路径，供播放器读取
+# 生成仓库所有音乐文件的路径，供播放器读取（ajax.js loadLocalMusicList）
 #  （可人工执行、亦可通过 Github Action 在 PR 时触发）
 # --------------------------------------------
 # usage: 
-#   python ./py/gen_music_list.py
+#   python ./py/gen_music_list.py -i {ignore_dir_keyword1,ignore_dir_keyword2,ignore_dir_keyword3,...}
 # --------------------------------------------
 
 
+import argparse
 import os
 import json
 import hashlib
@@ -25,7 +26,24 @@ LYRIC_SUFFIX = ".lrc"
 PIC_SUFFIX = ".jpg"
 
 
-def main() :
+def args() :
+    parser = argparse.ArgumentParser(
+        prog='', # 会被 usage 覆盖
+        usage='python ./py/gen_music_list.py -i {ignore_dir_keyword1,ignore_dir_keyword2,ignore_dir_keyword3,...}',  
+        description='生成 static 目录下的音乐歌单，允许跳过一些目录',  
+        epilog='\r\n'.join([
+            '更多参数执行', 
+            '  python ./py/gen_music_list.py -h', 
+            '查看', 
+        ])
+    )
+    parser.add_argument('-i', '--ignores', dest='ignores', type=str, default="", help='忽略目录列表（关键字即可），多个用英文逗号分隔')
+    return parser.parse_args()
+
+
+def main(args) :
+    ignores = tuple([x.strip() for x in args.ignores.split(',')])
+
     # 创建歌曲列表
     musiclist = MusicList(
         id="9527",
@@ -34,9 +52,14 @@ def main() :
         creatorName="EXP",
         creatorAvatar="images/avatar.jpg"
     )
+    log.info(f"开始生成歌单：【{musiclist.name}】")
 
     # 遍历所有文件
     for root, _, files in os.walk(MUSIC_DIR):
+        if root.lower().endswith(ignores) :
+            log.warn(f"跳过目录： {root}")
+            continue
+        
         for file in files:
             if not file.lower().endswith(tuple(MUSIC_SUFFIXES)) :
                 continue
@@ -77,6 +100,7 @@ def main() :
             musiclist.add(music)
 
     musiclist.save_to_file(MUSIC_LIST)
+    log.info(f"完成，共收录 {musiclist.size()} 首歌曲")
 
 
 def calculate_md5(file_path):
@@ -94,6 +118,9 @@ class MusicList:
 
     def add(self, music):
         self.item.append(music.__dict__)
+
+    def size(self) :
+        return len(self.item)
 
     def save_to_file(self, file_path):
         with open(file_path, 'w+', encoding=DEFAULT_ENCODING) as file:
@@ -115,4 +142,4 @@ class Music:
 
 
 if __name__ == "__main__" :
-    main()
+    main(args())
